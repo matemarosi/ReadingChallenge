44d7ce708e5f6d77f43da1ddfcbbf503239b76d0
Mate Marosi <mmate91@gmail.com>
2016-05-02 00:16:42 +0200

    Test SugarORM

---
+++ app/src/main/java/com/bme/aut/readingchallenge/model/Book.java
@@ -0,0 +1,16 @@
+package com.bme.aut.readingchallenge.model;
+
+import com.orm.SugarRecord;
+
+public class Book extends SugarRecord {
+    String title;
+    String edition;
+
+    public Book(){
+    }
+
+    public Book(String title, String edition){
+        this.title = title;
+        this.edition = edition;
+    }
+}
